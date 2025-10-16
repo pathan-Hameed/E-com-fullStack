8eeee2b38232c2b13f4c12b4e0ab21f62edbe9e1
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-10-16 21:01:47 +0530

    feat: built cart and placeOrder page

---
+++ frontend/src/components/CartTotal.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+
+function CartTotal() {
+
+    const {currrency, delivery_fee, getCartAmount} = useContext(ShopContext);
+
+
+  return (
+    <div className='w-full'>
+      <div className='text-2xl'>
+        <Title text1={"CART"} text2={"TOTAL"} />
+      </div>
+
+      <div className='flex flex-col gap-2 mt-2 text-sm'>
+        <div className='flex justify-between'>
+            <p>subtotal</p>
+            <p>{currrency} {getCartAmount()}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+            <p>Shipping Fee</p>
+            <p>{currrency} {delivery_fee}.00</p>
+        </div>
+        <hr />
+        <div className='flex justify-between'>
+            <b>Total</b>
+            <b>{currrency} {getCartAmount() === 0? 0 : getCartAmount() + delivery_fee}.00</b>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartTotal
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 function Cart() {
 
-  const { products, currency, cartItems} = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate} = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -50,12 +51,21 @@ function Cart() {
                   </div>
                 </div>
               </div>
-              <input className='border max-w-10 sm:max-w-20 px-1 py-1' type="number" min={1} defaultValue={item.quantity} />
-              <img className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="bin_icon" />
+              <input onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 py-1' type="number" min={1} defaultValue={item.quantity} />
+              <img onClick={()=>updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="bin_icon" />
             </div>
           )
 
         })}
+      </div>
+
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={()=>navigate('/place-order')} className='bg-black text-white px-4 py-2 my-8'>PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
       </div>
       
     </div>
