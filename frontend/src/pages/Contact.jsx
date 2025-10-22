13c6cecd35329de8a20b12c78b95cef226dcd81f
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-10-22 15:17:32 +0530

    feat: built the contact page ui, Completed the frontend part of the website

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 function Contact() {
   return (
     <div>
-      <div className=''></div>
+      <div className="text-2xl text-center pt-10 border-t">
+        <Title text1={"CONNECT"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="contact_img" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">54709 willms station <br /> Suite 350, Washington</p>
+          <p className="text-gray-500">Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
+          <p className="text-gray-600 font-semibold text-xl">Careers at Forever</p>
+          <p className="text-gray-500">Learn more about out teams and Job openings</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-75">Explore Jobs</button>
+        </div>
+      </div>
+
+      <NewsLetterBox />
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
