58783e20e79f1cc5ce64e2cec3cf24c9d27e7012
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-10-22 17:01:03 +0530

    feat: created backend folder structure, installed required dependencies, connected with mongodb, connected with cloudinary, created models, schemas for products and users.. current: working on routes for user login, user register, admin login

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+//Route for user registration
+const registerUser = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// Route for admin Login
+const adminLogin = async (req, res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin};
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongobd.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API endpoints
+app.use('/api/user', userRouter)
+
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log(`server started on PORT : ` + port))
