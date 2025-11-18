4f739bbc2cbf492472c02d73d816faeec89f6ec7
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-11-18 22:34:38 +0530

    user login feature built

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,66 +1,84 @@
-import validator from 'validator';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import userModel from '../models/userModel.js';
-
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 const createToken = (id) => {
-    return jwt.sign({id},process.env.JWT_SECRET)
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 // Route for user login
 const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-}
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } 
+  catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Route for user registration
 const registerUser = async (req, res) => {
-    try {
-        
-        const {name, email, password} = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        // checking uf user already exists or not
-        const exists = await userModel.findOne({email});
-        
-        if (exists) {
-            return res.json({success: false, message: "User already exists"});
-        }
+    // checking uf user already exists or not
+    const exists = await userModel.findOne({ email });
 
-        // validating email format & strong password
-        if (!validator.isEmail(email)) {
-            return res.json({success: false, message: "Invalid email format"});
-        }
-        if (password.length < 8) {
-            return res.json({success: false, message: "Password must be at least 8 characters long"});
-        }
-
-        // hashing user password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // creating new user
-        const newUser = new userModel({
-            name,
-            email,
-            password:hashedPassword
-        })
-
-        const user = await newUser.save();
-
-        const token = createToken(user._id)
-
-        res.json({success: true,token});
-
-    } catch (error) {
-        console.log(error);
-        res.json({success: false, message: error.message});
-    
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
     }
-}
+
+    // validating email format & strong password
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Invalid email format" });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password must be at least 8 characters long",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // creating new user
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Route for admin Login
-const adminLogin = async (req, res) => {
+const adminLogin = async (req, res) => {};
 
-}
-
-export {loginUser, registerUser, adminLogin};
+export { loginUser, registerUser, adminLogin };
