c84af25f9b713f736c6d8de1a4492d393a00c746
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-12-24 21:38:49 +0530

    feat:remove product, single product info, admin registration and login complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,6 +79,24 @@ const registerUser = async (req, res) => {
 };
 
 // Route for admin Login
-const adminLogin = async (req, res) => {};
+const adminLogin = async (req, res) => {
+  
+  try {
+    
+    const { email, password } = req.body;
+
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      const token = jwt.sign(email+password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid Admin Credentials" });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+
+};
 
 export { loginUser, registerUser, adminLogin };
